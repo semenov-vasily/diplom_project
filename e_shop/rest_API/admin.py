c9a539836d1a74b0513897235b8a2b2a3b0b386d
semenov vasily <vvs1771@yandex.ru>
2025-03-20 14:31:06 +0200

    admin panel added

--- e_shop/rest_API/admin.py
+++ e_shop/rest_API/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Supplier, Product, Cart, Contact, Order
 
-# Register your models here.
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Supplier._meta.fields]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Cart._meta.fields]
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Contact._meta.fields]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Order._meta.fields]
