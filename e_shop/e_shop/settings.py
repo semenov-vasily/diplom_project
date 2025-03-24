665382d76c60340b8e69d191a603cfe751410478
semenov vasily <vvs1771@yandex.ru>
2025-03-24 16:16:21 +0200

    fixes

--- e_shop/e_shop/settings.py
+++ e_shop/e_shop/settings.py
@@ -130,11 +130,9 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Пример, если вы используете Redis
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-# Если нужно, можно добавить и другие настройки
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
